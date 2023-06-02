8ab7fceb5814aabd0e3d34602f3338dd06d3809e
Guilherme Alves <guilhermealvescoc@gmail.com>
2023-06-02 14:53:00 -0300

    Upload

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -4,7 +4,7 @@ import '../../App.css'
 export class Btn extends Component {
     render() {
         return(
-            <a href='#' className='bg-green-main hover:bg-white text-purple-main text-sm rounded-full
+            <a href='#home' className='bg-green-main hover:bg-white text-purple-main text-sm rounded-full
                     px-10 py-3 uppercase font-bold max-w-xl mx-auto tracking-widest
                     transition duration-500'>Obtenha o Spotify Free</a>
         )
